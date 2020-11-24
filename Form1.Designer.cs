6a32f74297b414cd71caa277b35ec1b794a3686d
ishmam ali <ishmamalikhan@gmail.com>
2020-11-24 18:13:51 +0500

    Reminder Application

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Remainder_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblout = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.Validating += new System.ComponentModel.CancelEventHandler(this.Button1_Validating);
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(288, 173);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(180, 20);
+            this.text2.TabIndex = 1;
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(288, 124);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(180, 20);
+            this.text1.TabIndex = 2;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(228, 124);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(54, 13);
+            this.FirstName.TabIndex = 3;
+            this.FirstName.Text = "FirstName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "LastName";
+            // 
+            // lblout
+            // 
+            this.lblout.AutoSize = true;
+            this.lblout.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblout.ForeColor = System.Drawing.Color.Maroon;
+            this.lblout.Location = new System.Drawing.Point(342, 39);
+            this.lblout.Name = "lblout";
+            this.lblout.Size = new System.Drawing.Size(2, 15);
+            this.lblout.TabIndex = 5;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblout;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remainder_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String fName, lName;
+            int istVal,stVal,sum;
+                istVal = System.Convert.ToInt32(text1.Text);
+                stVal = System.Convert.ToInt32(text2.Text);
+                sum = istVal + stVal;
+            lblout.Text = "Total Value is " + sum.ToString();
+            
+           // MessageBox.Show("The Sum of tWo Number is " + sum);
+        }
+
+        private void Button1_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(text1.Text)){
+                e.Cancel = true;
+                errorProvider1.SetError(text1, "Please Enter a Value");
+                text1.Focus();
+            }
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+    }
+}
